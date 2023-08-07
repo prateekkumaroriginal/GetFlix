819e835cd51b030f615fbcbf7c9635de7cfd73ae
Prateek <prateek.kumar.original@gmail.com>
2023-08-08 02:02:16 +0530

    Put the pipeline for project

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import { useState, useEffect } from 'react'
 import fetchDataFromApi from './utils/api'
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration, getGenres } from './store/homeSlice';
+import Home from './pages/home/home';
+import Details from './pages/details/details';
+import PageNotFound from './pages/404/PageNotFound'
+import Explore from './pages/explore/Explore'
+import SeachResult from './pages/searchResult/SearchResult'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
     const dispatch = useDispatch()
-    const {url} = useSelector((state) => state.home)
+    const { url } = useSelector((state) => state.home)
 
     useEffect(() => {
         apiTesting();
---
+++ src/pages/explore/Explore.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.scss'
+
+const Explore = () => {
+  return (
+    <div>explore</div>
+  )
+}
+
+export default Explore
