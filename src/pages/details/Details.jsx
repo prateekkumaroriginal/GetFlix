429a485287793c2688b8c4ac1ed0a4da95f38a83
Prateek <prateek.kumar.original@gmail.com>
2023-08-13 13:55:26 +0530

    Add Top cast

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,7 @@ import './style.scss'
 import useFetch from '../../hooks/useFetch'
 import { useParams } from 'react-router-dom'
 import DetailsBanner from './detailsBanner/DetailsBanner'
+import Cast from './cast/Cast'
 
 const Details = () => {
     const { mediaType, id } = useParams()
@@ -12,6 +13,7 @@ const Details = () => {
     return (
         <div>
             <DetailsBanner video={data?.results[0]} crew={credits?.crew} />
+            <Cast data={credits?.cast} loading={creditsLoading} />
         </div>
     )
 }
